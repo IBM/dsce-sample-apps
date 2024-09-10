da9873df13f53c53a48dbf874903bdf2b8141e24
Zeel Patel <zeel.patel2@ibm.com>
2024-09-10 17:14:11 +0530

    updated title
    Signed-off-by: Zeel Patel <zeel.patel2@ibm.com>

--- energy-forecasting-using-granite-ttm/frontend/src/components/AppHeader/AppHeader.js
+++ energy-forecasting-using-granite-ttm/frontend/src/components/AppHeader/AppHeader.js
@@ -15,7 +15,7 @@ const AppHeader = ({iconClick}) => {
   }
   return (
         <Header aria-label="IBM Platform Name" className={styles.uiShellHeader}>
-          <HeaderName prefix="Energy Forecasting using granite TTM">
+          <HeaderName prefix="Energy forecasting using granite TTM">
           </HeaderName>
           <HeaderGlobalBar>
             <HeaderGlobalAction aria-label="Search" onClick={iconClickHandler}>
