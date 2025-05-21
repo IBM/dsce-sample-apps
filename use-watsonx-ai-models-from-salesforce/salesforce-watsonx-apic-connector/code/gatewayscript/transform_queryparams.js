85ea32e569016491cb744d3b5d41d70a517efe94
Ankit Katba <ankit.katba@ibm.com>
2025-05-21 14:39:37 +0530

    added new regions: au-syd, ca-tor
    Signed-off-by: Ankit Katba <ankit.katba@ibm.com>

--- use-watsonx-ai-models-from-salesforce/salesforce-watsonx-apic-connector/code/gatewayscript/transform_queryparams.js
+++ use-watsonx-ai-models-from-salesforce/salesforce-watsonx-apic-connector/code/gatewayscript/transform_queryparams.js
@@ -3,7 +3,8 @@ const qs = require("querystring");
 var parsedqs = qs.parse(context.request.querystring);
 
 console.error(
-  "=================================================================="
+  "==================================================================\nEndpoint: ",
+  context.get("request.uri")
 );
 
 var region = context.get("api.properties.wx-endpoint-us-south");
@@ -13,6 +14,12 @@ else if (parsedqs["region"] === "eu-de")
   region = context.get("api.properties.wx-endpoint-eu-de");
 else if (parsedqs["region"] === "jp-tok")
   region = context.get("api.properties.wx-endpoint-jp-tok");
+else if (parsedqs["region"] === "au-syd")
+  region = context.get("api.properties.wx-endpoint-au-syd");
+else if (parsedqs["region"] === "ca-tor")
+  region = context.get("api.properties.wx-endpoint-ca-tor");
+else if (parsedqs["region"] !== undefined)
+  region = "https://" + parsedqs["region"] + ".ml.cloud.ibm.com/ml/v1";
 
 var default_projectid = context.get("api.properties.default-project-id");
 context.set("wx.projectid", parsedqs["projectid"] || default_projectid);
