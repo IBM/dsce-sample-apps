0305128e2adacc83aef05a2af7ca73cb10085d6d
Suyash Gupte <gupte.suyash11@gmail.com>
2025-06-18 18:38:24 +0530

    add new host

--- wealth-managment-agent/frontend/vite.config.ts
+++ wealth-managment-agent/frontend/vite.config.ts
@@ -11,7 +11,8 @@ export default defineConfig({
    allowedHosts: ["wealth-manager-agent-v2.1s6emm0ktx8z.us-east.codeengine.appdomain.cloud", 
                   "dsce-test-ce-wtx-wealth-manager-single-agent-ui.1pde6bclcwl3.us-south.codeengine.appdomain.cloud",
                   "dsce-prod-ce-wtx-wealth-manager-single-agent-ui.1op8ay1afyb7.us-south.codeengine.appdomain.cloud",
-                  "58691dd33e.dsceapp.buildlab.cloud"
+                  "58691dd33e.dsceapp.buildlab.cloud",
+                  "wealth-frontend-v3.1wklpagrpdfa.us-south.codeengine.appdomain.cloud"
                  ]
   },
   server: {
